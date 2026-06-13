012ab36549cf78c0f9c95002e4f445d633541b14
Juan Entrena Garrido <jentrenagarr@protonmail.com>
2026-06-13 20:27:32 +0200

    fix: wrap overlay in TranslationProvider so useTranslation() doesn't crash
    Overlay entry point was missing TranslationProvider, causing all
    overlay components using useTranslation() (TimerOverlay, ChatOverlay,
    OverlayControls, etc.) to crash on mount because TranslationContext
    was null.

--- packages/frontend/src/overlay.tsx
+++ packages/frontend/src/overlay.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { OverlayContainer } from './components/OverlayContainer';
 import { OverlayErrorBoundary } from './components/OverlayErrorBoundary';
+import { TranslationProvider } from './i18n/context';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('overlay-root')!).render(
   <React.StrictMode>
-    <OverlayErrorBoundary>
-      <OverlayContainer />
-    </OverlayErrorBoundary>
+    <TranslationProvider>
+      <OverlayErrorBoundary>
+        <OverlayContainer />
+      </OverlayErrorBoundary>
+    </TranslationProvider>
   </React.StrictMode>,
 );
