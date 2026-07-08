8e0cf8a3baffd44d1c929e20f4c2e09bcf8453c5
Juan Entrena Garrido <jentrenagarr@protonmail.com>
2026-07-08 16:12:58 +0200

    feat: initialize backend server using Fastify and add jimp dependency

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -50,8 +50,9 @@ export async function startServer(opts?: { port?: number; frontendDir?: string }
       "default-src 'self'; " +
       "connect-src 'self' https://id.twitch.tv https://api.twitch.tv wss://eventsub.wss.twitch.tv ws://localhost:3000 ws://127.0.0.1:3000; " +
       "frame-src 'self' https://player.twitch.tv; " +
-      "img-src 'self' https://static-cdn.jtvnw.net data:; " +
-      "script-src 'self' 'unsafe-inline'; " +
+      "img-src 'self' https://static-cdn.jtvnw.net data: blob:; " +
+      "script-src 'self' 'unsafe-inline' blob:; " +
+      "worker-src 'self' blob:; " +
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
       "font-src 'self' https://fonts.gstatic.com"
     );
